9040e2f9000f3902f2b2590a3aecf5a71f36ad84
Ganesh Choudhary <gjat928@gmail.com>
2026-03-28 18:32:43 +0530

    fix: base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change 'ganesh-portfolio' to your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/gc-portfolio/',
+  base: '/',
 })
